8cdb87373639e650974f0f0e06cbc9c0e250a52c
Doug Liu <dougdliu@gmail.com>
2026-07-22 01:41:00 -0700

    Added superstructure with superstates

---
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+public class Superstructure {
+    /** Robot Superstates */
+    public enum Superstate {
+        DRIVE_STARTING_CONFIG,
+        DRIVE,
+        INTAKE,
+        OUTTAKE,
+        SHOOT_WITH_AIM,
+        SHOOT_NO_AIM,
+        MANUAL_SHOOT,
+        PURGE
+    }
+}
